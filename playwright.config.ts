b77daca4af488f538a62ba28247c923ce5646bb7
Gudivada <2472500@cognizant.com>
2026-03-30 18:15:50 +0530

    changes made

--- playwright.config.ts
+++ playwright.config.ts
@@ -14,16 +14,16 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
   testDir: './tests',
   /* Run tests in files in parallel */
-  fullyParallel: true,
+  fullyParallel: false,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry failed tests one time */
   retries: 1,
-//retries: process.env.CI ? 2 : 0,
+  //retries: process.env.CI ? 2 : 0,
 
   /* Opt out of parallel tests on CI. */
   workers: 1,
-// workers: process.env.CI ? 1 : undefined, 
+  // workers: process.env.CI ? 1 : undefined, 
 
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
@@ -52,18 +52,18 @@ export default defineConfig({
     ['./console-reporter.ts'],
     ['list']
   ],
-  
+
   /* Maximum time one test can run */
-  timeout: 300*1000,
-  
+  timeout: 300 * 1000,
+
   /* Maximum time for the entire test suite */
-  globalTimeout: 3600*1000,
-  
+  globalTimeout: 3600 * 1000,
+
   /* Maximum time for expect() assertions */
   expect: {
-    timeout: 10*1000
+    timeout: 10 * 1000
   },
-  
+
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
@@ -71,27 +71,27 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on',
-    
+
     /* Video recording for all tests */
     video: 'on',
-    
+
     /* Screenshot on failure */
     screenshot: 'on',
-    
+
     /* Maximum time for each action (click, fill, etc.) */
-    actionTimeout: 30*1000,
-    
+    actionTimeout: 30 * 1000,
+
     /* Maximum time for navigation actions */
-    navigationTimeout: 60*1000,
-    
-    headless: false,
+    navigationTimeout: 60 * 1000,
+
+    headless: true,
 
     // launchOptions:{
 
     // slowMo:800,
 
     //  }
-    
+
   },
 
   /* Configure projects for major browsers */
