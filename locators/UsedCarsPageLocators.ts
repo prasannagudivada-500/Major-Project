6d876ab74162a64cd0d5d8e6cea8c767b7b31103
Gudivada <2472500@cognizant.com>
2026-04-07 14:13:17 +0530

    Final commits are done succesfully

--- locators/UsedCarsPageLocators.ts
+++ locators/UsedCarsPageLocators.ts
@@ -9,8 +9,8 @@ export const UsedCarsPageLocators = {
   cityDropdownTitle: (cityName: string) => `title=${cityName}`, // Function typed with string param
   //   popularModelList: 'ul[class="zw-sr-secLev usedCarMakeModelList popularModels ml-20 mt-10"]',
   popularModelList: '.gsc_thin_scroll ul[class*="zw-sr-secLev"]',
-     scrollLocatorXPath: '//div[@class="zw-sr-shortWrap pt-15"]/div[@class="gsc_thin_scroll"]',
-  //scrollLocatorXPath: '.gsc_thin_scroll',
+  //scrollLocatorXPath: '//div[@class="zw-sr-shortWrap pt-15"]/div[@class="gsc_thin_scroll"]',
+  scrollLocatorXPath: '.gsc_thin_scroll',
   //   usedCarsHeaderXPath: '//h1[@id="usedcarttlID"]',
   usedCarsHeaderXPath: '#usedcarttlID'
 };
