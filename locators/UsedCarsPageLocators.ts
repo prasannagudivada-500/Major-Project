a47d0d587b20207895fb555c24a93644ffbd052b
Gudivada <2472500@cognizant.com>
2026-04-07 14:16:52 +0530

    Changes done

--- locators/UsedCarsPageLocators.ts
+++ locators/UsedCarsPageLocators.ts
@@ -10,6 +10,7 @@ export const UsedCarsPageLocators = {
   //   popularModelList: 'ul[class="zw-sr-secLev usedCarMakeModelList popularModels ml-20 mt-10"]',
   popularModelList: '.gsc_thin_scroll ul[class*="zw-sr-secLev"]',
   //scrollLocatorXPath: '//div[@class="zw-sr-shortWrap pt-15"]/div[@class="gsc_thin_scroll"]',
+
   scrollLocatorXPath: '.gsc_thin_scroll',
   //   usedCarsHeaderXPath: '//h1[@id="usedcarttlID"]',
   usedCarsHeaderXPath: '#usedcarttlID'
